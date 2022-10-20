a5c25bfde118981c0e4ff8a916c88e648927c636
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-21 02:30:50 +0530

    Day14-LinkedList-DS

---
+++ LinkedList.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day14.UC_Day14_LinkedList;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to LinkedList program");
+
+	}
+
+}
