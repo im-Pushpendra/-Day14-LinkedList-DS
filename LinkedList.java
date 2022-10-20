9113a8d4035c136c39d2e8f1741ba54e706abe04
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-21 02:33:02 +0530

    Added: create linked list logic

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,28 @@
 package com.bridgelabz.day14.UC_Day14_LinkedList;
 
 public class LinkedList {
+	Node head;
+	class Node
+	{
+		Object data;
+		Node ref;
+		Node(Object data)
+		{
+			this.data = data;
+		}
+	}
+	public void elements(Object data)
+	{
+		Node newNode = new Node(data);
+		System.out.print(newNode.data+" ");
+	}
 
 	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
 		System.out.println("Welcome to LinkedList program");
-
+		list.elements(56);
+		list.elements(30);
+		list.elements(70);
 	}
 
 }
